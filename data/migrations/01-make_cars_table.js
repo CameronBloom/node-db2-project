bd2936fb35bad7e3d943da1fbf8e433082d1c1ae
Cameron <cameron.morgan@skiff.com>
2023-09-14 19:35:25 -0400

    Completed project setup. Completed initial migrations and seeds.

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {
+    table.increments('id')
+    table.string('vin', 17).notNullable().unique()
+    table.string('make', 255).notNullable()
+    table.string('model', 255).notNullable()
+    table.integer('mileage').unsigned().notNullable()
+    table.string('title', 255)
+    table.string('transmission', 255)
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
-// STRETCH
+const cars = [
+  {
+    vin: "11111111111111111",
+    make: "Ford",
+    model: "Escape",
+    mileage: "75000",
+    title: "clean",
+    transmission: "automatic",
+  },
+  {
+    vin: "22222222222222222",
+    make: "Jeep",
+    model: "Renegade",
+    mileage: "15000",
+    title: "clean",
+    transmission: "automatic",
+  },
+  {
+    vin: "33333333333333333",
+    make: "BMW",
+    model: "Mini Cooper",
+    mileage: "12000",
+    title: "clean",
+    transmission: "automatic",
+  },
+]
+
+exports.seed = async function (knex) {
+  await knex('cars').truncate()
+  await knex('cars').insert(cars)
+}
